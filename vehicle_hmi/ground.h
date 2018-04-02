c5e75235e323ee16e20f3fa3bb26c4e4bd3b0d19
peter <xe@kthx.de>
2018-04-03 01:12:22 +0200

    Fix shadowing of grid members

--- vehicle_hmi/ground.h
+++ vehicle_hmi/ground.h
@@ -15,15 +15,13 @@ class Ground final : public apeiron::engine::Entity
 public:
   Ground(const glm::vec3& size, const glm::vec3& spacing, float precision)
       : grid_{size, spacing, precision} {}
-  glm::vec3 size() const { return size_; }
-  glm::vec3 spacing() const { return spacing_; }
+  glm::vec3 size() const { return grid_.size(); }
+  glm::vec3 spacing() const { return grid_.spacing(); }
   void render() const override { grid_.render(); }
 
 
 private:
   apeiron::opengl::Grid grid_;
-  glm::vec3 size_;
-  glm::vec3 spacing_;
 };
 
 

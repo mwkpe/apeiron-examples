12ac9330c028b6eac94250a7e713b4709a6b68d8
peter <xe@kthx.de>
2018-04-19 01:48:53 +0200

    Refactor code

--- vehicle_hmi/world.cpp
+++ vehicle_hmi/world.cpp
@@ -9,31 +9,52 @@ void hmi::World::init()
 {
   namespace mf = apeiron::engine::model_flags;
 
+  set_camera(0);
   renderer_.init();
+  roboto_mono_.load("assets/roboto_mono_modified.png");
 
   bulb_.load("assets/bulb.obj", mf::vertices);
+  light_.set_position(0.0f, 5.0f, 1.0f);
+  light_.set_color(1.0f, 1.0f, 1.0f);
+  renderer_.set_light_color(light_.color());
 
   ego_vehicle_.load_model("assets/bmw.obj", mf::vertices | mf::normals | mf::tex_coords);
   ego_vehicle_.load_texture("assets/bmw.png");
   ego_vehicle_.set_center(0.0f, ego_vehicle_.size().y / 2.0f, 0.0f);
-  ego_vehicle_.set_color({0.129f, 0.588f, 0.952f, 1.0f});
+  ego_vehicle_.set_color({0.611f, 0.152f, 0.690f, 1.0f});
 
   target_vehicle_.load_model("assets/audi.obj", mf::vertices | mf::normals | mf::tex_coords);
   target_vehicle_.load_texture("assets/audi.png");
-  target_vehicle_.set_position(5.0f, 0.0f, 55.0f);
+  target_vehicle_.set_position(3.6f, 0.0f, 65.0f);
   target_vehicle_.set_center(0.0f, target_vehicle_.size().y / 2.0f, 0.0f);
   target_vehicle_.set_color({0.956f, 0.262f, 0.211f, 1.0f});
 
-  light_.set_position(0.0f, 5.0f, 1.0f);
-  light_.set_color(1.0f, 1.0f, 1.0f);
-
-  renderer_.set_light_color(light_.color());
+  roman_.set_text("Roman");
+  roman_.set_position(0.0f, -1.0f, 0.0f);
+  roman_.set_spacing(0.95f, 1.0f);
 }
 
 
 void hmi::World::reset()
 {
   camera_.reset();
+}
+
+
+void hmi::World::set_camera(int i)
+{
+  switch (i) {
+    case 0:
+      camera_.set({0.0f, 40.0f, 30.0f}, -50.0f, -90.0f);
+      break;
+    case 1:
+      camera_.set({0.0f, 25.0f, 40.0f}, -32.0f, -90.0f);
+      break;
+    case 2:
+      camera_.set({25.0f, 40.0f, 40.0f}, -40.0f, -125.0f);
+      break;
+    default:;
+  }
 }
 
 
@@ -57,13 +78,19 @@ void hmi::World::update([[maybe_unused]] float time, float delta_time, const ape
   if (options_->vehicle_velocity != ego_vehicle_.velocity())
     ego_vehicle_.set_velocity(options_->vehicle_velocity);
 
+  deviation_meter_.set_longitudinal_deviation(options_->distance_deviation);
+  deviation_meter_.update(delta_time);
+  deviation_gauge_.set_value(options_->distance_deviation);
+  target_position_.set_position(0.0f, options_->distance_deviation);
+  velocity_gauge_.update(options_->vehicle_velocity, options_->target_velocity);
+
   float ground_z = ground_.position().z;
   ground_z += delta_time * (ego_vehicle_.velocity());
   if (ground_z > ground_.spacing().z)
     ground_z -= ground_.spacing().z;
   if (ground_z < -ground_.spacing().z)
     ground_z += ground_.spacing().z;
-  ground_.set_position(0.0f, 0.0f, ground_z);
+  ground_.set_position(0.0f, -0.1f, ground_z);
 
   float line_z = line_markings_.position().z;
   float line_spacing = line_markings_.line_spacing();
@@ -73,9 +100,6 @@ void hmi::World::update([[maybe_unused]] float time, float delta_time, const ape
   if (line_z < -line_spacing)
     line_z += line_spacing;
   line_markings_.set_position(0.0f, 0.0f, line_z);
-
-  deviation_meter_.set_deviation(options_->distance_deviation);
-  target_position_.set_position(0.0f, options_->distance_deviation);
 }
 
 
@@ -83,7 +107,6 @@ void hmi::World::render()
 {
   frame_++;
   auto aspect_ratio = static_cast<float>(options_->window_width) / options_->window_height;
-
   renderer_.set_projection(glm::perspective(glm::radians(45.0f), aspect_ratio, 2.5f, 500.0f));
   renderer_.set_view(camera_.view());
   renderer_.set_wireframe(options_->wireframe);
@@ -97,17 +120,21 @@ void hmi::World::render()
   }
 
   renderer_.use_color_shading();
+
   if (options_->road) {
     renderer_.render(road_, road_.color());
     renderer_.render(line_markings_, line_markings_.color());
+    renderer_.render(roman_, roboto_mono_);
   }
 
   if (options_->ground_overlay) {
-    renderer_.use_color_shading();
-    renderer_.render(deviation_meter_, deviation_meter_.color());
-    auto c = deviation_meter_.color();
-    c.a = 1.0f;
-    renderer_.render(target_position_, c);
+    deviation_meter_.render(renderer_, options_->animate_overlay);
+    renderer_.render(target_position_, deviation_meter_.longitudinal_color());
+  }
+
+  if (options_->bounding_boxes) {
+    renderer_.render_bounds(ego_vehicle_, ego_vehicle_.color());
+    renderer_.render_bounds(target_vehicle_, target_vehicle_.color());
   }
 
   renderer_.use_texture_shading();
@@ -117,10 +144,6 @@ void hmi::World::render()
   renderer_.set_light_position(target_vehicle_.position() + light_.position());
   renderer_.render(target_vehicle_);
 
-  if (options_->bounding_boxes) {
-    renderer_.set_lighting(false);
-    renderer_.use_color_shading();
-    renderer_.render_bounds(ego_vehicle_, ego_vehicle_.color());
-    renderer_.render_bounds(target_vehicle_, target_vehicle_.color());
-  }
+  deviation_gauge_.render(renderer_);
+  velocity_gauge_.render(renderer_);
 }
--- vehicle_hmi/world.h
+++ vehicle_hmi/world.h
@@ -6,6 +6,7 @@
 #include <vector>
 #include "engine/input.h"
 #include "engine/camera.h"
+#include "engine/text.h"
 #include "opengl/model.h"
 #include "opengl/renderer.h"
 #include "options.h"
@@ -14,8 +15,12 @@
 #include "line_markings.h"
 #include "position_marker.h"
 #include "deviation_meter.h"
+#include "deviation_animation.h"
+#include "gauge.h"
 #include "light.h"
 #include "vehicle.h"
+#include "deviation_gauge.h"
+#include "velocity_gauge.h"
 
 
 namespace hmi {
@@ -25,25 +30,34 @@ class World final
 {
 public:
   World(const Options* options) : options_{options},
-      ground_{{30.0f, 0.0f, 400.0f}, {2.5f, 0.0f, 2.5f}, 0.01f, {0.05f, 0.05f, 0.05f}},
+      roboto_mono_{16, 8, 32},
+      ground_{{40.0f, 0.0f, 400.0f}, {2.5f, 0.0f, 5.0f}, 0.01f, {0.3f, 0.3f, 0.3f}},
+      deviation_gauge_{&roboto_mono_},
+      velocity_gauge_{&roboto_mono_},
       light_{&bulb_},
       ego_vehicle_{{1.847f, 1.271f, 4.131f}},
-      target_vehicle_{{1.916f, 1.266f, 4.396f}} {}
+      target_vehicle_{{1.916f, 1.266f, 4.396f}}
+      {}
   void init();
   void reset();
+  void set_camera(int i);
   void update(float time, float delta_time, const apeiron::engine::Input* input = nullptr);
   void render();
 
 private:
   const Options* options_;
   apeiron::opengl::Renderer renderer_;
+  apeiron::opengl::Charset roboto_mono_;
   apeiron::opengl::Model bulb_;
   apeiron::engine::Camera camera_;
+  apeiron::engine::Text roman_;
   Ground ground_;
   Road road_;
   Line_markings line_markings_;
   Position_marker target_position_;
   Deviation_meter deviation_meter_;
+  Deviation_gauge deviation_gauge_;
+  Velocity_gauge velocity_gauge_;
   Light light_;
   Vehicle ego_vehicle_;
   Vehicle target_vehicle_;

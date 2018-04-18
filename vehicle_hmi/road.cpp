efb872499f3940be4c0fe422d0b6c5af3b295505
peter <xe@kthx.de>
2018-04-19 01:46:41 +0200

    Increase width

--- vehicle_hmi/road.cpp
+++ vehicle_hmi/road.cpp
@@ -3,7 +3,7 @@
 
 hmi::Road::Road() : road_{1.0f, 1.0f}
 {
-  set_position(0.0f, -0.1f, 0.0f);
-  set_scale(7.6f, 1.0f, 400.0f);
+  set_position(0.0f, -0.05f, 0.0f);
+  set_scale(11.2f, 1.0f, 400.0f);
   set_rotation(glm::radians(-90.0f), 0.0f, 0.0f);
 }

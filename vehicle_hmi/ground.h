d86e48d4e029b69e48e627f7609dd890b91ee23f
peter <xe@kthx.de>
2018-04-19 01:48:05 +0200

    Move ground below 0

--- vehicle_hmi/ground.h
+++ vehicle_hmi/ground.h
@@ -14,7 +14,7 @@ class Ground final : public apeiron::engine::Entity
 {
 public:
   Ground(const glm::vec3& size, const glm::vec3& spacing, float precision, const glm::vec3& color,
-      float line_width = 1.0f) : grid_{size, spacing, precision, color, line_width} {}
+      float line_width = 1.0f) : grid_{size, spacing, precision, color, line_width} { set_position(0.0f, -0.1f, 0.0f); }
   glm::vec3 size() const { return grid_.size(); }
   glm::vec3 spacing() const { return grid_.spacing(); }
   void render() const override { grid_.render(); }

3a03103f86d41994bab4b943829d6d733ca7312f
peter <pemacij@gmail.com>
2018-09-25 11:33:58 +0200

    Refactor code

--- point_cloud/source/point_cloud.cpp
+++ point_cloud/source/point_cloud.cpp
@@ -22,20 +22,17 @@ void example::Point_cloud::load_data(std::string_view filename)
         float x = std::stof(values[1]);
         float y = std::stof(values[2]);
         float z = std::stof(values[3]);
-        float r = std::stof(values[4]);
-        float g = std::stof(values[5]);
-        float b = std::stof(values[6]);
-        float a = std::stof(values[7]);
-        data.push_back(Vertex_color{
-            // Move cloud to center
-            x + 30.0f,
-            y + 625.0f,
-            z - 128.0f,
-            // Negative values indicate missing color information, use fuchsia
-            r < 0 ? 1.0f : r,
-            g < 0 ? 0.0f : g,
-            b < 0 ? 1.0f : b,
-            a < 0 ? 1.0f : a});
+
+        // Negative values indicate missing color information, use fuchsia
+        if (float r = std::stof(values[4]); r < 0.0f) {
+          data.push_back(Vertex_color{x + 30.0f, y + 625.0f, z - 128.0f, 1.0f, 0.0f, 1.0f, 1.0f});
+        }
+        else {
+          float g = std::stof(values[5]);
+          float b = std::stof(values[6]);
+          float a = std::stof(values[7]);
+          data.push_back(Vertex_color{x + 30.0f, y + 625.0f, z - 128.0f, r, g, b, a});
+        }
       }
     }
 

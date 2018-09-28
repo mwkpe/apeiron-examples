952883e6ff6ab092e61be51d11b99585e55329cd
peter <pemacij@gmail.com>
2018-09-28 19:27:23 +0200

    Add black/white flag

--- chess_game/source/piece.cpp
+++ chess_game/source/piece.cpp
@@ -1,7 +1,7 @@
 #include "piece.h"
 
 
-example::chess::Piece::Piece(Type type, const apeiron::opengl::Model* model)
-    : type_{type}, model_{model}
+example::chess::Piece::Piece(Type type, Chess_color color, const apeiron::opengl::Model* model)
+    : type_{type}, chess_color_{color}, model_{model}
 {
 }
--- chess_game/source/piece.h
+++ chess_game/source/piece.h
@@ -13,12 +13,18 @@ class Piece final : public apeiron::engine::Entity
 {
 public:
   enum class Type { Pawn, Knight, Bishop, Rook, Queen, King };
-  explicit Piece(Type type, const apeiron::opengl::Model* model = nullptr);
+  enum class Chess_color { White, Black };
+  explicit Piece(Type type, Chess_color color, const apeiron::opengl::Model* model = nullptr);
   void set_model(const apeiron::opengl::Model* model) { model_ = model; }
+  void set_type(Type type) { type_ = type; }
+  void set_chess_color(Chess_color color) { chess_color_ = color; }
+  Type type() const { return type_; }
+  Chess_color chess_color() const { return chess_color_; }
   void render() const override { model_->render(); }
 
 private:
   Type type_ = Type::Pawn;
+  Chess_color chess_color_ = Chess_color::White;
   const apeiron::opengl::Model* model_ = nullptr;
 };
 
